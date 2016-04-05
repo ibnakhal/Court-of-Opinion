b1b62789d282c9f862ea02c0d40253eb2a8c4649
ibnakhal <INAKHAL2@art.edu>
2016-04-04 19:28:12 -0700

    additional crimes added

--- Assets/Scripts/WordSystem.cs
+++ Assets/Scripts/WordSystem.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System;
 public class WordSystem : MonoBehaviour {
     [SerializeField]
     private Text crimeText;
@@ -21,14 +22,39 @@ public class WordSystem : MonoBehaviour {
         Sentence();
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-	if(Input.GetKeyDown("e"))
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("e"))
         {
             Generate();
         }
-	}
+        string inputThisFrame = Input.inputString;
+        try
+        {
+
+            int inputAsInt = Convert.ToInt32(inputThisFrame);
+
+            if (inputAsInt >= 1 && inputAsInt <= m_sentence.Length)
+            {
+                sentenceText.text = "You are sentenced to " + m_sentence[inputAsInt - 1].sentence;
+                for (int x = 0; x < m_sentence[inputAsInt - 1].punishment.Length; x++)
+                {
+                    sentenceText.text += "\n" + (x + 1) + " : " + m_sentence[inputAsInt - 1].punishment[x];
+                    // sentenceText.text += "\n" + "2 : " + m_sentence[inputAsInt - 1].punishment[1] + "\n" + "3 : " + m_sentence[inputAsInt - 1].punishment[2]);
+                }
+
+
+
+
+
+            }
+        }
+        catch (FormatException)
+        {
+            //did not hit a number key
+        }
+    }
 
     public void Sentence()
     {
@@ -37,9 +63,9 @@ public class WordSystem : MonoBehaviour {
 
     public void Generate()
     {
-        int rando = Random.Range(0, m_crime.Length);
-        int cRando = Random.Range(0, m_crime[rando].committance.Length);
-        int sRando = Random.Range(0, m_crime[rando].subject.Length);
+        int rando = UnityEngine.Random.Range(0, m_crime.Length);
+        int cRando = UnityEngine.Random.Range(0, m_crime[rando].committance.Length);
+        int sRando = UnityEngine.Random.Range(0, m_crime[rando].subject.Length);
         crimeText.text = ("" + opener + " " + m_crime[rando].committance[cRando] + " " + m_crime[rando].subject[sRando]);
    }
 }
